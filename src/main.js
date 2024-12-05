bab2a5e83c5e6e856c44841df091487d08740bcc
Antoine NAVETTE <antoinenavette@gmail.com>
2024-12-05 13:42:12 +0100

    Ajout du mode sombre

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-import './style.css'
+import './style.css';
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#000000');
+}
