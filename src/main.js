d6d55c49ffbe81b2889a09aac1028c5692cb68c2
Antoine NAVETTE <antoinenavette@gmail.com>
2024-12-06 11:15:26 +0100

    Ajout du plateau de jeu

--- src/main.js
+++ src/main.js
@@ -1,15 +1,1 @@
 import './style.css';
-
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#000000');
-}
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-    const isDarkMode = event.matches;
-    const themeColorMeta = document.querySelector('meta[name="theme-color"]');
-    if (isDarkMode) {
-        themeColorMeta.setAttribute('content', '#000000');
-    } else {
-        themeColorMeta.setAttribute('content', '#ffffff');
-    }
-});
